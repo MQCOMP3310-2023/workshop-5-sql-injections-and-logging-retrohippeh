794e5f01c5cfce935bbad81e15fae7475a2c1709
Retrohippeh <lolochlan@gmail.com>
2023-03-20 16:45:30 +1100

    comit to test for sec issues

--- app/src/main/java/workshop05code/SQLiteConnectionManager.java
+++ app/src/main/java/workshop05code/SQLiteConnectionManager.java
@@ -145,7 +145,9 @@ public class SQLiteConnectionManager {
      * @return true if guess exists in the database, false otherwise
      */
     public boolean isValidWord(String guess) {
-        String sql = "SELECT count(id) as total FROM validWords WHERE word like'" + guess + "';";
+        
+        
+        String sql = "SELECT count(id) as total FROM validWords WHERE word like '?';";
 
         try (Connection conn = DriverManager.getConnection(databaseURL);
                 PreparedStatement stmt = conn.prepareStatement(sql)) {
